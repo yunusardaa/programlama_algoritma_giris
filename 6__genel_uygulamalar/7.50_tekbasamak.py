837e56c307beff93399aebf1088330061760657c
yunusardaa <96135315+yunusardaa@users.noreply.github.com>
2021-12-27 12:26:50 +0300

    Create 7.50_tekbasamak.py

---
+++ 6__genel_uygulamalar/7.50_tekbasamak.py
@@ -0,0 +1,7 @@
+# tek basamak
+r=["sıfır","bir","iki","üç","dört","beş","altı","yedi","sekiz","dokuz"]
+x=int(input("tek basamaklı tam sayı giriniz: "))
+print("\ngiriniz sayı => ",end='')
+if (x<0):
+   print("eksi ",end='')
+print(r[abs(x)])   
